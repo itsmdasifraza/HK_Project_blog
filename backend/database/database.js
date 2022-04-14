0a92624b4d2dfa5b2659c1ccad96bf11e3c5ac8b
Md Asif Raza <itsmdasifraza@gmail.com>
2022-04-14 20:01:49 +0530

    connected app with mongoDB server

---
+++ backend/database/database.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+const mongoUrl = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00.s4uhv.mongodb.net:27017,cluster0-shard-00-01.s4uhv.mongodb.net:27017,cluster0-shard-00-02.s4uhv.mongodb.net:27017/${process.env.MONGO_DATABASE}?ssl=true&replicaSet=atlas-haaizc-shard-0&authSource=admin&retryWrites=true&w=majority`;
+//const mongoUrl = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00.5oa06.mongodb.net:27017,cluster0-shard-00-01.5oa06.mongodb.net:27017,cluster0-shard-00-02.5oa06.mongodb.net:27017/${process.env.MONGO_DATABASE}?ssl=true&replicaSet=atlas-lruunc-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+mongoose.connect(mongoUrl , (err,data)=>{
+    if(err){ console.log(err); }
+    if(data){ console.log("Node Application Started Successfully!"); }
+});
